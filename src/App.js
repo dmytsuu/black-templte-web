ce86d2fe1be94a1e6360fe3b6c751d26a302e0e8
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-08-02 02:01:31 +0300

    Add realmlist info

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import Form from './Form.js'
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <Form/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      address: '',
+      port: '',
+      name: ''
+    }
+  };
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/realmlist')
+    .then((response => {
+      this.setState(response.data)
+    }))
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Form/>
+        <div className="realmlist">
+          <p>Realm: { this.state.name }</p>
+          { `set realmlist ${this.state.address}:${this.state.port}` }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
